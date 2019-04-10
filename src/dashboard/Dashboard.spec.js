9465d2e9e9e1fd2fcde95972860a42ed6e073881
Sem8 <mssemmi8@gmail.com>
2019-04-10 16:45:00 -0400

    snapshot of Display and Dashboard components tests passed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import renderer from "react-test-renderer";
+
+import Dashboard from "./Dashboard.js";
+
+afterEach(() => {
+  cleanup();
+});
+
+describe.skip("<Dashboard />", () => {
+  it("matches snapshot of Dashboard", () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import renderer from "react-test-renderer";
+
+import Display from "./Display.js";
+
+describe.skip("<Display />", () => {
+  it("matches snapshot of Display", () => {
+    const tree = renderer.create(<Display />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
